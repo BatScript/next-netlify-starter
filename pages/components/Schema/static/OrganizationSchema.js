fe4fe8c8069a0c71bd2b138484abecfc3c8bd278
mohit <mohit80940@gmail.com>
2022-11-30 09:06:30 +0530

    kill me NOWWWWWW

--- pages/components/Schema/static/OrganizationSchema.js
+++ pages/components/Schema/static/OrganizationSchema.js
@@ -1,24 +1,25 @@
 import React from "react";
+import Script from "next/script";
 
 const createOrganizationSchema = {
     "@context": "http://schema.org",
     "@type": "Organization",
-    "name": "Lybrate ",
-    "logo": "https://www.lybrate.com/img/lybrate-logo-pure-white-small.png",
-    "telephone": "090  2905  9702",
-    "url": "https://www.lybrate.com/",
-    "email": "hello@lybrate.com",
-    "sameAs": ["https://twitter.com/lybrate", "https://plus.google.com/+Lybrate/", "https://www.facebook.com/lybrate", "https://www.linkedin.com/company/lybrate"],
-    "contactPoint": {
+    name: "Lybrate ",
+    logo: "https://www.lybrate.com/img/lybrate-logo-pure-white-small.png",
+    telephone: "090  2905  9702",
+    url: "https://www.lybrate.com/",
+    email: "hello@lybrate.com",
+    sameAs: ["https://twitter.com/lybrate", "https://plus.google.com/+Lybrate/", "https://www.facebook.com/lybrate", "https://www.linkedin.com/company/lybrate"],
+    contactPoint: {
         "@type": "ContactPoint",
-        "telephone": "+91 90155 59900",
-        "contactType": "customer service",
+        telephone: "+91 90155 59900",
+        contactType: "customer service",
     },
 };
 
 const OrganizationSchema = () => {
     return (
-        <script
+        <Script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
                 __html: JSON.stringify(createOrganizationSchema),
